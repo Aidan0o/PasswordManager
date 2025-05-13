f720fd96afe67840edf4088042645a88f1e2db9f
Willthetitan <william.thomas.official21@gmail.com>
2025-05-13 15:19:14 +0100

    IN CLASS CHANGES
    Changes in File Structure and removal of middleman to ensure a simplified process

--- User Interface and C code/db.js
+++ User Interface and C code/db.js
@@ -1,16 +1,3 @@
-const express = require("express"); //run cmd to make node work, then 'npm install' in terminal to calll dependant node functions, when calling new functions run 'npm install FunctionName --save'
-const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.listen(3000, () => console.log("Listening on http://localhost:3000/LoginPage.html"));//when the node is run, opens the data on a server
-app.use(express.static("./FrontEnd"));
-async function getData(request, response){
-    receive=5//This needs to be connected to the database
-    console.log(receive);
-    response.send(
-        receive//this is what's returned to the function in PasswordJava
-  )
-}
 
 
 
@@ -87,5 +74,4 @@ function verifyUser(name, password) {
 // Test login
 // verifyUser('alice', 'mypassword123'); //
 // verifyUser('alice', 'wrongpassword'); //
-verifyUser('BILLYBOBBY', 'DELTASIER');
-app.post("/get-data", getData);//This makes the function getData read from another file and output the data into it
+verifyUser('BILLYBOBBY', 'DELTASIERRA');
